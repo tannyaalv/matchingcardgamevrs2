91d2be3f0c5e6858809c97cef2f508556adffd03
Tanya Alvarez-Salsedo <tanyaalvsal@tamu.edu>
2024-04-24 10:10:11 -0500

    Initial Commit

---
+++ matchingcardgamevrs2/matchingcardgamevrs2App.swift
@@ -0,0 +1,17 @@
+//
+//  matchingcardgamevrs2App.swift
+//  matchingcardgamevrs2
+//
+//  Created by Alvarez-Salsedo, Tanya on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct matchingcardgamevrs2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
